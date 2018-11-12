bc03900c2139db1a22a24c0871c1c826978076ad
kumar-raakesh <kmrrakesh21@gmail.com>
2018-11-12 14:47:00 +0530

    Add files via upload

---
+++ react/build/precache-manifest.7ea1d376e06c6390396f2eed971b9ba3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "69835525ba3e014d29d1",
+    "url": "/static/js/main.69835525.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "69835525ba3e014d29d1",
+    "url": "/static/css/main.9520459c.chunk.css"
+  },
+  {
+    "revision": "4c027d9795800add2451b7935aefb98e",
+    "url": "/index.html"
+  }
+];
